9db0a51f17333ce136ca26bd71436a93e2f9f402
Srđan Vidović <vidosrd@gmail.com>
2026-04-27 22:14:53 +0200

    1

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails/all"
 Bundler.require(*Rails.groups)
 
-module Aa
+module Ar
   class Application < Rails::Application
     config.load_defaults 7.1
     config.autoload_lib(ignore: %w(assets tasks))
